669ef5829b1e70da02f40232d2ac3206ab1ec617
songxf <songxf@huaxindata.com.cn>
2018-07-10 09:20:01 +0800

    提交新建mylibrary

---
+++ mylibrary/src/main/java/testgradle/sxf/com/mylibrary/Test.java
@@ -0,0 +1,13 @@
+package testgradle.sxf.com.mylibrary;
+
+/**
+ * @author songxf
+ * @date 2018/7/10
+ * @Desc $desc$
+ * Copyright HUAXIN DATA. All rights reserved.
+ */
+public class Test {
+    public double addition(double addnum1, double addnum2) {
+        return addnum1 + addnum2;
+    }
+}
